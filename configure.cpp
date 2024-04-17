49a1bf76767782f23fc344fbfbb033ceccbe5072
joligej <jasper-desmaele@outlook.com>
2024-04-17 12:46:40 +0200

    Made configure program more advanced and removed library and user settings file from repo

--- configure.cpp
+++ configure.cpp
@@ -1,132 +1,146 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+#include <filesystem>
 
-int main() {
-    std::ofstream config_file (USER_SETTINGS);
+#define REPEAT(x) for (decltype(x) i = 0; i < x; ++i)
+
+int main (int argc, char * argv[]) {
+    const bool DEFAULT_STATE = argc > 1 && strcmp(argv[1], "DEFAULT") == 0;
     
-    std::cout << "\nCurrently writing to: " << USER_SETTINGS << '\n';
+    if (DEFAULT_STATE) REPEAT(7) std::cin.putback('\n');
     
-    config_file << "#include <cstdint> // for uint*_t\n\n";
-    
+    if (DEFAULT_STATE) {
+        std::filesystem::copy(DEFAULT_USER_SETTINGS, USER_SETTINGS);
+    } else {
+        
+        std::ofstream config_file (USER_SETTINGS);
+        
+        std::cout << "\nCurrently writing to: " << USER_SETTINGS << '\n';
+        
+        config_file << "#include <cstdint> // for uint*_t\n\n";
+        
     DEFAULT_BASE:
-    
-    std::cout << "\nWhat would you like your default base to be?\nIf you don't enter anything, it will default to 10.\nEnter base: ";
-    
-    std::string default_base;
-    std::getline(std::cin, default_base);
-    
-    config_file << "#define DEFAULT_BASE (unsigned)";
-    if (default_base.empty()) config_file << 10;
-    else {
-        try {
-            std::stoull(default_base);
-        } catch (...) {
-            std::cout << "\nOops, that didn't work. Please try again.\n";
-            goto DEFAULT_BASE;
+        
+        std::cout << "\nWhat would you like your default base to be?\nIf you don't enter anything, it will default to 10.\nEnter base: ";
+        
+        std::string default_base;
+        std::getline(std::cin, default_base);
+        
+        config_file << "#define DEFAULT_BASE (unsigned)";
+        if (default_base.empty()) config_file << 10;
+        else {
+            try {
+                std::stoull(default_base);
+            } catch (...) {
+                std::cout << "\nOops, that didn't work. Please try again.\n";
+                goto DEFAULT_BASE;
+            }
+            config_file << default_base;
         }
-        config_file << default_base;
-    }
-    
-    config_file << '\n';
-    
-    std::cout << "\nWhat would you like your base type to be?\nIf you don't enter anything, it will default to uint16_t.\nEnter type: ";
-    
-    std::string base_int;
-    std::getline(std::cin, base_int);
-    
-    config_file << "#define BASE_INT ";
-    if (base_int.empty()) config_file << "uint16_t";
-    else config_file << base_int;
-    
-    config_file << '\n';
-    
-    std::cout << "\nWhat would you like your bigger-than-base-type type to be?\nIf you don't enter anything, it will default to uint32_t.\nEnter type: ";
-    
-    std::string base_int_big;
-    std::getline(std::cin, base_int_big);
-    
-    config_file << "#define BASE_INT_BIG ";
-    if (base_int_big.empty()) config_file << "uint32_t";
-    else config_file << base_int_big;
-    
-    config_file << '\n';
-    
+        
+        config_file << '\n';
+        
+        std::cout << "\nWhat would you like your base type to be?\nIf you don't enter anything, it will default to uint16_t.\nEnter type: ";
+        
+        std::string base_int;
+        std::getline(std::cin, base_int);
+        
+        config_file << "#define BASE_INT ";
+        if (base_int.empty()) config_file << "uint16_t";
+        else config_file << base_int;
+        
+        config_file << '\n';
+        
+        std::cout << "\nWhat would you like your bigger-than-base-type type to be?\nIf you don't enter anything, it will default to uint32_t.\nEnter type: ";
+        
+        std::string base_int_big;
+        std::getline(std::cin, base_int_big);
+        
+        config_file << "#define BASE_INT_BIG ";
+        if (base_int_big.empty()) config_file << "uint32_t";
+        else config_file << base_int_big;
+        
+        config_file << '\n';
+        
     BASE:
-    
-    std::cout << "\nWhat is the base corresponding to your base type?\nIf you don't enter anything, it will default to 65,536.\nEnter base: ";
-    
-    std::string base;
-    std::getline(std::cin, base);
-    
-    config_file << "#define BASE (BASE_INT_BIG)";
-    if (base.empty()) config_file << "65'536";
-    else {
-        try {
-            std::stoull(base);
-        } catch (...) {
-            std::cout << "\nOops, that didn't work. Please try again.\n";
-            goto BASE;
+        
+        std::cout << "\nWhat is the base corresponding to your base type?\nIf you don't enter anything, it will default to 65,536.\nEnter base: ";
+        
+        std::string base;
+        std::getline(std::cin, base);
+        
+        config_file << "#define BASE (BASE_INT_BIG)";
+        if (base.empty()) config_file << "65'536";
+        else {
+            try {
+                std::stoull(base);
+            } catch (...) {
+                std::cout << "\nOops, that didn't work. Please try again.\n";
+                goto BASE;
+            }
+            config_file << base;
         }
-        config_file << base;
-    }
-    
-    config_file << '\n';
-    
+        
+        config_file << '\n';
+        
     MAX_RATIO:
-    
-    std::cout << "\nWhat is the ratio between your smallest type and your base type?\nIf you don't enter anything, it will default to 0.000125.\nMake sure to enter a ratio a little smaller.\nEnter ratio: ";
-    
-    std::string max_ratio;
-    std::getline(std::cin, max_ratio);
-    
-    config_file << "#define MAX_RATIO (double)";
-    if (max_ratio.empty()) config_file << "0.000125";
-    else {
-        try {
-            std::stold(max_ratio);
-        } catch (...) {
-            std::cout << "\nOops, that didn't work. Please try again.\n";
-            goto MAX_RATIO;
+        
+        std::cout << "\nWhat is the ratio between your smallest type and your base type?\nIf you don't enter anything, it will default to 0.000125.\nMake sure to enter a ratio a little smaller.\nEnter ratio: ";
+        
+        std::string max_ratio;
+        std::getline(std::cin, max_ratio);
+        
+        config_file << "#define MAX_RATIO (double)";
+        if (max_ratio.empty()) config_file << "0.000125";
+        else {
+            try {
+                std::stold(max_ratio);
+            } catch (...) {
+                std::cout << "\nOops, that didn't work. Please try again.\n";
+                goto MAX_RATIO;
+            }
+            config_file << max_ratio;
         }
-        config_file << max_ratio;
-    }
-    
-    config_file << '\n';
-    
-    std::cout << "\nWhat is the type you would like to use to count bits?\nIf you don't enter anything, it will default to unsigned long long.\nEnter type: ";
-    
-    std::string bit_type;
-    std::getline(std::cin, bit_type);
-    
-    config_file << "#define BIT_TYPE ";
-    if (bit_type.empty()) config_file << "unsigned long long";
-    else config_file << bit_type;
-    
-    config_file << '\n';
-    
+        
+        config_file << '\n';
+        
+        std::cout << "\nWhat is the type you would like to use to count bits?\nIf you don't enter anything, it will default to unsigned long long.\nEnter type: ";
+        
+        std::string bit_type;
+        std::getline(std::cin, bit_type);
+        
+        config_file << "#define BIT_TYPE ";
+        if (bit_type.empty()) config_file << "unsigned long long";
+        else config_file << bit_type;
+        
+        config_file << '\n';
+        
     BITS_IN_BYTE:
-    
-    std::cout << "\nWhat is the amount of bits in a byte on this particular system?\nIf you don't enter anything, it will default to 8.\nEnter number: ";
-    
-    std::string bits_in_byte;
-    std::getline(std::cin, bits_in_byte);
-    
-    config_file << "#define BITS_IN_BYTE (BIT_TYPE)";
-    if (bits_in_byte.empty()) config_file << "8";
-    else {
-        try {
-            std::stoull(bits_in_byte);
-        } catch (...) {
-            std::cout << "\nOops, that didn't work. Please try again.\n";
-            goto BITS_IN_BYTE;
+        
+        std::cout << "\nWhat is the amount of bits in a byte on this particular system?\nIf you don't enter anything, it will default to 8.\nEnter number: ";
+        
+        std::string bits_in_byte;
+        std::getline(std::cin, bits_in_byte);
+        
+        config_file << "#define BITS_IN_BYTE (BIT_TYPE)";
+        if (bits_in_byte.empty()) config_file << "8";
+        else {
+            try {
+                std::stoull(bits_in_byte);
+            } catch (...) {
+                std::cout << "\nOops, that didn't work. Please try again.\n\n";
+                goto BITS_IN_BYTE;
+            }
+            config_file << bits_in_byte;
         }
-        config_file << bits_in_byte;
+        
+        config_file << '\n';
+        
     }
     
-    config_file << '\n';
-    
-    std::cout << "\nConfiguration succesful!\n\n";
+    std::cout << "Configuration succesful\n";
     
     return 0;
 }
--- makefile
+++ makefile
@@ -1,30 +1,31 @@
 LibName = jmaths
+LibFileName = lib$(LibName).a
+HeaderName = header.hpp
 
-USER_SETTINGS = config_file.cfg
+UserSettings = user_settings.cfg
+DefaultUserSettings = default_user_settings.cfg
 
 CC = clang++
-
 CompileVersion = -std=c++2b
-
 CompileWarnings = -Wall -Werror -Wextra -Wpedantic -Wreturn-std-move -Wpessimizing-move -Wdefaulted-function-deleted
-
 CompileOptimisation = -O3 -march=native -flto
 
 CompileParms = $(CompileVersion) $(CompileWarnings) $(CompileOptimisation)
 
 HeaderObjs = jmaths.hpp jmaths_tmpl.cpp user_settings.cfg jmaths_def.cfg jmaths_undef.cfg
-
 ImplObjs = jmaths_N.cpp jmaths_Z.cpp jmaths_Q.cpp jmaths_calc.cpp jmaths_misc.cpp jmaths_error.cpp jmaths_literals.cpp
 
-.PHONY: clean
+.PHONY: all fresh clean library header install
 
 all:
-	@$(MAKE) configure && ./configure && $(MAKE) library
+	@$(MAKE) configure && ./configure DEFAULT && $(MAKE) library && $(MAKE) header
 	
+fresh:
+	@$(MAKE) clean && $(MAKE) all
 
 configure: configure.cpp
 	@echo "Creating a configuration program..."
-	@$(CC) $(CompileVersion) $(CompileOptimisation) -DUSER_SETTINGS="\"$(USER_SETTINGS)\"" configure.cpp -o configure
+	@$(CC) $(CompileVersion) $(CompileOptimisation) -DUSER_SETTINGS="\"$(UserSettings)\"" -DDEFAULT_USER_SETTINGS="\"$(DefaultUserSettings)\"" $< -o $@
 	@echo "Configuration program created"
 
 jmaths.o: $(ImplObjs) $(HeaderObjs)
@@ -33,9 +34,14 @@ jmaths.o: $(ImplObjs) $(HeaderObjs)
 	@echo "Library compiled succesfully"
 
 clean:
-	@rm -f *.o configure $(USER_SETTINGS)
+	@rm -f configure $(UserSettings) *.o $(LibFileName) $(HeaderName)
 
 library: jmaths.o
 	@echo "Archiving the library..."
-	@ar rcs lib$(LibName).a jmaths.o
+	@ar rcs $(LibFileName) jmaths.o
 	@echo "Library archived succesfully"
+	
+header: $(HeaderObjs)
+	@echo "Creating a header..."
+	@$(CC) $(CompileVersion) $< -E > $(HeaderName)
+	@echo "Header created succesfully"
