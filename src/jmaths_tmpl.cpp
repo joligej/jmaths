1c1a563311bea704b47f1b1ed94009b931b0e2c5
joligej <jasper-desmaele@outlook.com>
2024-04-24 14:44:04 +0200

    Edited previously mentioned mistake in other place too

--- src/jmaths_tmpl.cpp
+++ src/jmaths_tmpl.cpp
@@ -323,7 +323,7 @@ bool detail::opr_eq (const Q & lhs, FLOAT rhs) {
     
     // it is assumed here that exponent is negative at this point
     
-    Q helper ((std::make_unsigned_t<decltype(numerator)>)numerator, (std::make_unsigned_t<decltype(denominator)>)denominator);
+    Q helper ((std::make_unsigned_t<decltype(numerator)>)numerator, (std::make_unsigned_t<decltype(denominator)>)denominator, sign);
     
     return (lhs == helper);
 }
