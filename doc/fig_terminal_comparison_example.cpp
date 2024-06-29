06e70e9b1e889833fbf6363a4130a1fbaf611ce8
joligej <jasper-desmaele@outlook.com>
2024-06-30 00:03:15 +0200

    Added code for first example in documentation

---
+++ doc/fig_terminal_comparison_example.cpp
@@ -0,0 +1,17 @@
+#include "/usr/local/jmaths/jmaths.hpp"
+#include <iostream>
+
+int main() {
+    std::string input_str;
+    std::cout << "Enter your number:\n";
+    std::cin >> input_str;
+
+    using jmaths::Z, jmaths::calc;
+
+    Z z (input_str);
+    std::cout << "z == " << z << '\n';
+    ++z;
+    std::cout << "z + 1 == " << z << '\n';
+    z = calc::pow(z, 2); // or z *= z;
+    std::cout << "(z + 1)^2 == " << z << '\n';
+}
