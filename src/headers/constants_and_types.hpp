1e89a2bf846d8e252593a5ecdc8cfeddd4834fed
joligej <jasper-desmaele@outlook.com>
2024-07-08 22:14:31 +0200

    Changed one word in comment

--- src/headers/constants_and_types.hpp
+++ src/headers/constants_and_types.hpp
@@ -31,7 +31,7 @@ inline constexpr base_int_big base = 256;
 
 inline constexpr char negative_sign = '-'; // the sign that precedes negative numbers
 inline constexpr char vinculum = '/'; // the fraction bar that separates the numerator and denominator of a fraction
-using bit_type = unsigned long long; // the type that should store a number of bits, preferably the largest type available
+using bit_type = unsigned long long; // the type that should store a count of bits, preferably the largest type available
 inline constexpr unsigned default_base = 10; // the default base that strings are assumed to be in
 template <typename T> using allocator = std::allocator<T>; // the allocator used to store the numbers in a container on the heap
 
